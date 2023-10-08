671d7cfe38ab862a406ae0f04e83bb06d7ea3b1c
DavinW <49922027+DavinWam@users.noreply.github.com>
2023-10-08 01:38:27 -0400

    camera look ahead
    camera looks slightly ahead of player movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,13 +5,17 @@ public class CameraController : MonoBehaviour
     public Transform target;  // Player's Transform
     public Vector2 offset;    // Offset from the player
     public float smoothSpeed = 0.125f;  // Dampening factor for smooth movement
+    public float lookAheadFactor = 0.5f;  // How much the camera should look ahead
+    private Vector2 lookAhead;
+    private Vector2 targetLookAhead;
+    private Vector2 currentVelocity;
 
     private bool shouldClampPosition = false;
 
     private void FixedUpdate()
     {
-        Vector2 desiredPosition = (Vector2)target.position + offset;
-        Vector2 smoothedPosition = Vector2.Lerp((Vector2)transform.position, desiredPosition, smoothSpeed);
+        Vector2 desiredPosition = (Vector2)target.position + offset + lookAhead;
+        Vector2 smoothedPosition = Vector2.SmoothDamp((Vector2)transform.position, desiredPosition, ref currentVelocity, smoothSpeed);
         transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, transform.position.z);
         shouldClampPosition = true;
     }
@@ -23,19 +27,10 @@ public class CameraController : MonoBehaviour
             transform.position = PixelPerfectUtility.ClampToPixelGrid(transform.position);
             shouldClampPosition = false;
         }
-    }
 
-    private float CalculateUnitsPerPixel()
-    {
-        float cameraHeight = Camera.main.orthographicSize * 2; 
-        return cameraHeight / Screen.height;
-    }
-
-    private Vector3 ClampToPixelGrid(Vector3 position)
-    {
-        float unitsPerPixel = CalculateUnitsPerPixel();
-        float x = Mathf.Round(position.x / unitsPerPixel) * unitsPerPixel;
-        float y = Mathf.Round(position.y / unitsPerPixel) * unitsPerPixel;
-        return new Vector3(x, y, position.z);
+        // Look ahead logic
+        Vector2 moveDirection = target.GetComponent<Rigidbody2D>().velocity.normalized;
+        targetLookAhead = moveDirection * lookAheadFactor;
+        lookAhead = Vector2.Lerp(lookAhead, targetLookAhead, smoothSpeed);
     }
 }
