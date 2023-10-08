65ec8f903cd274e377035c0206d1d8c4e6baf7b9
Cory Scaffidi <ccscaffidi@gmail.com>
2023-10-08 14:50:35 -0400

    More flexible map interaction possible and resetting

--- Assets/Scripts/Map/MapInteraction.cs
+++ Assets/Scripts/Map/MapInteraction.cs
@@ -13,34 +13,69 @@ public class MapInteraction : MonoBehaviour
     public string sceneName;
     public bool enableSceneSwitch = true;
     public bool startDisabled = true;
-    private GameObject level;
+    private GameObject parent;
+    private bool isSelected = false;
+    private bool defaultState = true;
+    private Sprite disabledSprite = null;
 
     public void Start() {
         if(startDisabled) {
             Button btn = gameObject.GetComponent<Button>();
             if(btn != null) btn.interactable = false;
         }
-        level = gameObject.transform.parent.gameObject;
     }
 
-    public void parentUnlocked() {
+    public void parentUnlocked(GameObject newParent) {
+        parent = newParent;
         gameObject.GetComponent<Button>().interactable = true;
+        defaultState = false;
+    }
+
+    public void lockChildren() {
+        foreach(GameObject child in children) {
+            child.GetComponent<Button>().interactable = false;
+        }
     }
 
     public void onClick() {
+        isSelected = true;
+        defaultState = false;
         Button btn = gameObject.GetComponent<Button>();
         SpriteState tmpState = btn.spriteState;
         tmpState.disabledSprite = btn.spriteState.selectedSprite;
+        disabledSprite = btn.spriteState.disabledSprite;
         btn.spriteState = tmpState;
-        foreach(Transform child in level.transform) {
-            child.gameObject.GetComponent<Button>().interactable = false;
-        }
+        if(parent != null)
+            parent.GetComponent<MapInteraction>().lockChildren();
+        else
+            btn.interactable = false;
         foreach(GameObject child in children) {
-            child.GetComponent<MapInteraction>().parentUnlocked();
+            child.GetComponent<MapInteraction>().parentUnlocked(gameObject);
         }
         if(enableSceneSwitch)
             SceneManager.LoadScene(sceneName);
         else
             Debug.Log(sceneName + " Activated");
+    }
+
+    public void resetTree() {
+        if(defaultState) return;
+        Button btn = gameObject.GetComponent<Button>();
+        if(isSelected) {
+            isSelected = false;
+            SpriteState tmpState = btn.spriteState;
+            tmpState.disabledSprite = disabledSprite;
+            btn.spriteState = tmpState;
+        }
+        if(startDisabled) {
+            btn.interactable = false;
+        }
+        else {
+            btn.interactable = true;
+        }
+        foreach(GameObject child in children) {
+            child.GetComponent<MapInteraction>().resetTree();
+        }
+        defaultState = true;
     }
 }
