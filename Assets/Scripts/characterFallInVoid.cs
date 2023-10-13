57c95d7ad85f0732ab998b02378122be409df2d8
Cory Scaffidi <ccscaffidi@gmail.com>
2023-10-13 16:29:47 -0400

    Improved character fall in void script

--- Assets/Scripts/characterFallInVoid.cs
+++ Assets/Scripts/characterFallInVoid.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 public class characterFallInVoid : MonoBehaviour
 {
     public GameObject playerObject;
+    public GameObject cameraObject;
+    public float pauseSeconds = 0.2f;
+    public float pauseSecondsBeforeMove = 0.1f;
     public bool returnToClosestHorizontal = true;
     public bool returnToClosestVertical = false;
     public float displacement = 0.5f;
     public Vector3 alternativeReturnPos;
 
+    private bool realligning = false;
+
+    IEnumerator pauseForEffect(Vector3 returnPos) {
+        realligning = true;
+        Time.timeScale = 0;
+        CameraFade fade = cameraObject.GetComponent<CameraFade>();
+
+        fade.fadeOut(1.0f / pauseSecondsBeforeMove);
+        yield return new WaitForSecondsRealtime(pauseSecondsBeforeMove);
+
+        playerObject.transform.position = returnPos;
+            
+        fade.fadeIn(1.0f / (pauseSeconds * 2));
+        yield return new WaitForSecondsRealtime(pauseSeconds);
+
+        Time.timeScale = 1;
+        realligning = false;
+    }
     void OnTriggerStay2D(Collider2D col) {
-        if(col.gameObject == playerObject) {
+        if(col.gameObject == playerObject && !realligning) {
             // possibly remove health
             Bounds bnds = GetComponent<BoxCollider2D>().bounds;
             if(!bnds.Contains(col.bounds.max) || !bnds.Contains(col.bounds.min)) return;
@@ -32,8 +53,8 @@ public class characterFallInVoid : MonoBehaviour
             else {
                 returnPos = alternativeReturnPos;
             }
-            playerObject.transform.position = returnPos;
 
+            StartCoroutine(pauseForEffect(returnPos));
         }
     }
 }
