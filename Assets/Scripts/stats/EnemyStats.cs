cb1584eaa0c3e2dadb2e9b5929218858f976176c
DavinW <49922027+DavinWam@users.noreply.github.com>
2023-10-14 22:30:21 -0400

    made damage numbers

--- Assets/Scripts/stats/EnemyStats.cs
+++ Assets/Scripts/stats/EnemyStats.cs
@@ -1,19 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class EnemyStats : CharacterStats
 {
+ public GameObject damageTextPrefab;  // Assign your prefab in the Inspector
+    public float floatSpeed = 1f;  // Speed of floating upwards
+    public float fadeDuration = 1f;  // Duration of fading out
+    public Vector3 offset = new Vector3(0, -1f, 0f); // Offset from the enemy's position where the text starts
+
+    private Canvas canvas;
+
+    private void Awake()
+    {
+        canvas = GetComponentInChildren<Canvas>();
+        if(!canvas)
+        {
+            Debug.LogError("No Canvas child found on the GameObject!");
+        }
+    }
+
     public override void TakeDamage(int damage)
     {
         base.TakeDamage(damage);
 
-        // Enemy-specific damage logic, if any
-        // Note: The base.TakeDamage already reduces health and checks for death.
-        // Destroy the enemy object after handling the above logic.
-        if(this.currentHealth <= 0){
+        ShowDamageText(damage);
+
+        if(this.currentHealth <= 0)
+        {
             Destroy(gameObject);
         }
+    }
+
+    void ShowDamageText(int damage)
+    {
+        GameObject textObject = Instantiate(damageTextPrefab, transform.position + offset, Quaternion.identity, canvas.transform);
+
+        TMP_Text tmpText = textObject.GetComponent<TMP_Text>();
+        tmpText.text = damage.ToString();
+
+        StartCoroutine(FloatingAndFade(tmpText));
+    }
+
+    IEnumerator FloatingAndFade(TMP_Text tmpText)
+    {
+        float elapsed = 0f;
+        Color originalColor = tmpText.color;
+
+        while (elapsed < fadeDuration)
+        {
+            tmpText.transform.position += Vector3.up * floatSpeed * Time.deltaTime;
+
+            float alpha = Mathf.Lerp(1f, 0f, elapsed / fadeDuration);
+            tmpText.color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(tmpText.gameObject);
     }
 
 }
